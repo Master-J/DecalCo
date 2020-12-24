de342c868fbe7ccd6e6c3cf02d7b38354d642513
miziziziz <gauranarayana@gmail.com>
2020-12-24 16:51:26 -0700

    removed need to have separate instance of each decal and made so you don't need a script on them, except on one shot flipbook decals

---
+++ decalco/src/SetStartTimeOnDecal.gd
@@ -0,0 +1,10 @@
+extends MeshInstance
+
+# use this script on one shot flipbook animations
+
+func _ready():
+	var cur_time = OS.get_ticks_msec() / 1000.0
+	var mat = get_surface_material(0).duplicate(true)
+	set_surface_material(0, mat)
+	mat.set_shader_param("start_time", cur_time)
+	
